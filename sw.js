0d5b82009fae2ecb9fbdd4e3ae87ccb0b5d5ee7c
krisxdoohan-hub <krisx.doohan@gmail.com>
2026-05-28 00:05:37 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'budget-app-v0.0.0.1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/@phosphor-icons/web'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Cache hit
+        }
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                // Don't cache POST requests or external API calls here generally, but our ASSETS_TO_CACHE are GET
+                if(event.request.method === "GET") {
+                  cache.put(event.request, responseToCache);
+                }
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
